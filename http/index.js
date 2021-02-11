f598c9a4e1d5ba50023d8ecb06e5ce53de2d9576
youme <mrsimplelife@naver.com>
2021-02-11 21:43:46 +0900

    add index.html

--- http/index.js
+++ http/index.js
@@ -1,10 +1,23 @@
 const http = require("http");
-
+const fs = require("fs").promises;
 const server = http
-  .createServer((req, res) => {
-    res.write("<h1>Hello</h1>");
-    res.write("<p>Hello</p>");
-    res.end("<p>Hello</p>");
+  .createServer(async (req, res) => {
+    try {
+      res.writeHead(200, {
+        "Content-Type": "text/html; charset=utf-8",
+      });
+      const data = await fs.readFile("./index.html");
+      res.end(data);
+    } catch (error) {
+      res.writeHead(200, {
+        "Content-Type": "text/plain; charset=utf-8",
+      });
+      res.end(error.message);
+    }
+
+    // res.write("<h1>Hello</h1>");
+    // res.write("<p>Hello</p>");
+    // res.end("<p>Hello</p>");
   })
   .listen(8080);
 server.on("listening", () => {
