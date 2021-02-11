9863c7023af9a39a03cd70ec7d4ee947cdb4d899
youme <mrsimplelife@naver.com>
2021-02-11 18:58:16 +0900

    Review buffer and stream

--- fs/index.js
+++ fs/index.js
@@ -1,15 +1,47 @@
-const fs = require("fs").promises;
+const fs = require("fs");
+const writeStream = fs.createWriteStream("./wirtebuffer.txt");
+writeStream.on("finish", () => {
+  console.log("finish");
+});
+writeStream.write("ok write\n");
+writeStream.write("ok hihihihi\n");
+writeStream.end();
+// const readStream = fs.createReadStream("./testbuffer.txt", {
+//   highWaterMark: 16,
+// });
+// const data = [];
+// readStream.on("data", (chunk) => {
+//   data.push(chunk);
+//   console.log("data: ", chunk, chunk.length);
+// });
+// readStream.on("error", (err) => {
+//   throw err;
+// });
+// readStream.on("end", () => {
+//   console.log("end: ", Buffer.concat(data).toString());
+// });
+// const buffer = Buffer.from("change me to buffer");
+// console.log(buffer);
+// console.log(buffer.length);
+// console.log(buffer.toString());
 
-(async () => {
-  let data = await fs.readFile("./test.txt");
-  console.log("1", data.toString());
-  data = await fs.readFile("./test.txt");
-  console.log("2", data.toString());
-  data = await fs.readFile("./test.txt");
-  console.log("3", data.toString());
-  data = await fs.readFile("./test.txt");
-  console.log("4", data.toString());
-})();
+// const arr = [Buffer.from("hi "), Buffer.from("bye "), Buffer.from("merong")];
+// console.log(Buffer.concat(arr).toString());
+
+// console.log(Buffer.alloc(5));
+
+// const fs = require("fs").promises;
+
+// (async () => {
+//   let data = await fs.readFile("./test.txt");
+//   console.log("1", data.toString());
+//   data = await fs.readFile("./test.txt");
+//   console.log("2", data.toString());
+//   data = await fs.readFile("./test.txt");
+//   console.log("3", data.toString());
+//   data = await fs.readFile("./test.txt");
+//   console.log("4", data.toString());
+// })();
 // fs.readFile("./test.txt")
 //   .then((data) => {
 //     console.log("1", data);
