ca2cc6c3db6b5f661bb20dac461195ef8255fe89
youme <mrsimplelife@naver.com>
2021-02-16 20:30:18 +0900

    Add middleware

--- express/index.js
+++ express/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.set("port", process.env.PORT || 3000);
+app.use((req, res, next) => {
+  console.log("all req");
+  next();
+});
 app.get("/", (req, res) => {
   // res.send("hi");
   res.sendFile(path.join(__dirname, "index.html"));
@@ -9,10 +13,18 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   res.send("hi post");
 });
+app.get("/category/:name", (req, res) => {
+  res.send(`${req.params.name}`);
+});
+app.get("/category/java", (req, res) => {
+  res.send(`java hoho`);
+});
 app.get("/about", (req, res) => {
   res.send("about");
 });
-
+app.get("*", (req, res) => {
+  res.send("all!");
+});
 app.listen(app.get("port"), () => {
   console.log(`http://localhost:${process.env.PORT || 3000}`);
 });
