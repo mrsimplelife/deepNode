30c20d84412f74a4a7446fa626a9b12e81e2ed19
youme <mrsimplelife@naver.com>
2021-02-16 18:54:19 +0900

    Add send html file

--- express/index.js
+++ express/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 app.set("port", process.env.PORT || 3000);
 app.get("/", (req, res) => {
-  res.send("hi");
+  // res.send("hi");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 app.post("/", (req, res) => {
   res.send("hi post");
