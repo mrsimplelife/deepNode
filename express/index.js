82a8dae9daaf770a5b7503905be7c91814b4c1b9
youme <mrsimplelife@naver.com>
2021-02-16 20:45:38 +0900

    Add 404, error middleware

--- express/index.js
+++ express/index.js
@@ -6,6 +6,20 @@ app.use((req, res, next) => {
   console.log("all req");
   next();
 });
+app.use(
+  "/about",
+  (req, res, next) => {
+    console.log("only about req");
+    next();
+  },
+  (req, res, next) => {
+    console.log("one more");
+    next();
+  },
+  (req, res, next) => {
+    throw new Error("error!!!");
+  }
+);
 app.get("/", (req, res) => {
   // res.send("hi");
   res.sendFile(path.join(__dirname, "index.html"));
@@ -22,8 +36,19 @@ app.get("/category/java", (req, res) => {
 app.get("/about", (req, res) => {
   res.send("about");
 });
-app.get("*", (req, res) => {
-  res.send("all!");
+// app.get("*", (req, res) => {
+//   res.send("all!");
+// });
+// const a = (a1) => {};
+// const b = (a1, a2) => {};
+// console.log(a.length, b.length);
+
+app.use((req, res) => {
+  res.status(404).send("404");
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("error!!!");
 });
 app.listen(app.get("port"), () => {
   console.log(`http://localhost:${process.env.PORT || 3000}`);
